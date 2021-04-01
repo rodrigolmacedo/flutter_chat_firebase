5f97971fb35c8d5d1730a077f9dfb5493f6b433f
Rodrigo <7695045+rodrigolmacedo@users.noreply.github.com>
2021-04-01 20:59:56 -0300

    melhorando o tema

--- flutter_chat_firebase/lib/main.dart
+++ flutter_chat_firebase/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_chat_firebase/screens/auth_screen.dart';
 import 'package:flutter_chat_firebase/screens/chat_screen.dart';
 
 Future<void> main() async {
@@ -15,9 +16,19 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Chat',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: ChatScreen(),
+          primarySwatch: Colors.pink,
+          backgroundColor: Colors.pink,
+          accentColor: Colors.deepPurple,
+          accentColorBrightness: Brightness.dark,
+          buttonTheme: ButtonTheme.of(context).copyWith(
+            buttonColor: Colors.pink,
+            textTheme: ButtonTextTheme.primary,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(20),
+            ),
+          ),
+          visualDensity: VisualDensity.adaptivePlatformDensity),
+      home: AuthScreen(),
     );
   }
 }
