a5b3f5b148a6f67bcdd8dc0b126be48b840ad6af
Rodrigo <7695045+rodrigolmacedo@users.noreply.github.com>
2021-04-01 20:59:35 -0300

    evoluindo formulario de login/cadastro

--- flutter_chat_firebase/lib/widgets/auth_form.dart
+++ flutter_chat_firebase/lib/widgets/auth_form.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_chat_firebase/models/auth_data.dart';
 
 class AuthForm extends StatefulWidget {
   @override
@@ -6,6 +7,17 @@ class AuthForm extends StatefulWidget {
 }
 
 class _AuthFormState extends State<AuthForm> {
+  final GlobalKey<FormState> _formKey = GlobalKey();
+  final AuthData _authData = AuthData();
+
+  _submit() {
+    bool isValid = _formKey.currentState.validate();
+
+    if (isValid) {
+      print(_authData);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -14,23 +26,67 @@ class _AuthFormState extends State<AuthForm> {
         child: SingleChildScrollView(
           padding: EdgeInsets.all(16),
           child: Form(
+              key: _formKey,
               child: Column(
-            children: [
-              TextFormField(
-                decoration: InputDecoration(labelText: 'Nome'),
-              ),
-              TextFormField(
-                decoration: InputDecoration(labelText: 'E-mail'),
-              ),
-              TextFormField(
-                obscureText: true,
-                decoration: InputDecoration(labelText: 'Senha'),
-              ),
-              SizedBox(height: 12),
-              ElevatedButton(onPressed: () {}, child: Text('Entrar')),
-              TextButton(onPressed: () {}, child: Text('Criar nova conta?'))
-            ],
-          )),
+                children: [
+                  if (_authData.isSignup)
+                    TextFormField(
+                      key: ValueKey('name'),
+                      initialValue: _authData.name,
+                      decoration: InputDecoration(labelText: 'Nome'),
+                      onChanged: (value) => _authData.name = value,
+                      validator: (value) {
+                        if (value == null || value.trim().length < 4) {
+                          return "Nome inválido";
+                        }
+
+                        return null;
+                      },
+                    ),
+                  TextFormField(
+                    key: ValueKey('email'),
+                    initialValue: _authData.email,
+                    decoration: InputDecoration(labelText: 'E-mail'),
+                    onChanged: (value) => _authData.email = value,
+                    validator: (value) {
+                      if (value == null || !value.trim().contains('@')) {
+                        return "E-mail inválido";
+                      }
+
+                      return null;
+                    },
+                  ),
+                  TextFormField(
+                    key: ValueKey('password'),
+                    initialValue: _authData.password,
+                    obscureText: true,
+                    decoration: InputDecoration(labelText: 'Senha'),
+                    onChanged: (value) => _authData.password = value,
+                    validator: (value) {
+                      if (value == null || value.trim().length < 7) {
+                        return "Senha inválido";
+                      }
+
+                      return null;
+                    },
+                  ),
+                  SizedBox(height: 12),
+                  ElevatedButton(
+                      style: ButtonStyle(),
+                      onPressed: _submit,
+                      child: Text(_authData.isLogin ? 'Entrar' : 'Cadastrar')),
+                  TextButton(
+                    child: Text(_authData.isLogin
+                        ? 'Criar nova conta?'
+                        : 'Já possui uma conta?'),
+                    onPressed: () {
+                      setState(() {
+                        _authData.toggleMode();
+                      });
+                    },
+                  )
+                ],
+              )),
         ),
       ),
     );
