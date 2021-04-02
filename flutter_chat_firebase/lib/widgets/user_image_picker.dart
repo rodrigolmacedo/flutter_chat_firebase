75878c992544c025ddb82245f71740616f27e358
Rodrigo <7695045+rodrigolmacedo@users.noreply.github.com>
2021-04-02 10:11:15 -0300

    adaptações para rodar na web

--- flutter_chat_firebase/lib/widgets/user_image_picker.dart
+++ flutter_chat_firebase/lib/widgets/user_image_picker.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 
@@ -28,6 +29,14 @@ class _UserImagePickerState extends State<UserImagePicker> {
     widget.onImagePick(_pickedImageFile);
   }
 
+  ImageProvider carregarImagem() {
+    if (kIsWeb) {
+      return NetworkImage(_pickedImageFile.path);
+    } else {
+      return FileImage(File(_pickedImageFile.path));
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -35,8 +44,7 @@ class _UserImagePickerState extends State<UserImagePicker> {
         CircleAvatar(
           radius: 40,
           backgroundColor: Colors.grey,
-          backgroundImage:
-              _pickedImageFile != null ? FileImage(_pickedImageFile) : null,
+          backgroundImage: _pickedImageFile != null ? carregarImagem() : null,
         ),
         TextButton.icon(
             onPressed: _pickImage,
