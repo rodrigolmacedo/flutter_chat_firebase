6bf2f861664c062429ea714149028105fc7242dd
Rodrigo <7695045+rodrigolmacedo@users.noreply.github.com>
2021-04-02 12:41:22 -0300

    adicionando firebasemessaging ao projeto

--- flutter_chat_firebase/lib/main.dart
+++ flutter_chat_firebase/lib/main.dart
@@ -1,17 +1,89 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_chat_firebase/screens/auth_screen.dart';
 import 'package:flutter_chat_firebase/screens/chat_screen.dart';
 
-Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
+//topLevelFunction
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  // If you're going to use other Firebase services in the background, such as Firestore,
+  // make sure you call `initializeApp` before using other Firebase services.
   await Firebase.initializeApp();
+
+  print("Handling a background message: ${message.messageId}");
+}
+
+Future<void> main() async {
+  if (kIsWeb) {
+    FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+    WidgetsFlutterBinding.ensureInitialized();
+  } else {
+    WidgetsFlutterBinding.ensureInitialized();
+    FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+  }
+  await Firebase.initializeApp();
+
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   // This widget is the root of your application.
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  FirebaseMessaging messaging = FirebaseMessaging.instance;
+  Future<void> subscribeTopic() async {
+    if (!kIsWeb) await FirebaseMessaging.instance.subscribeToTopic('novidades');
+  }
+
+  Future<void> requestPermission() async {
+    NotificationSettings settings = await messaging.requestPermission(
+      alert: true,
+      announcement: false,
+      badge: true,
+      carPlay: false,
+      criticalAlert: false,
+      provisional: false,
+      sound: true,
+    );
+
+    print('User granted permission: ${settings.authorizationStatus}');
+  }
+
+  Future<void> requestToken() async {
+    String token;
+    if (kIsWeb) {
+      token = await messaging.getToken(
+        vapidKey:
+            "BPt6MS8PpevPDzUrUrEt5QqXw3u3dUs3nD3n3DJWy431QYXlK4MJnNA4umRQL4F6AOnefPbCfU279WC7dqYHBnI",
+      );
+    } else {
+      token = await messaging.getToken();
+    }
+
+    print('User token generated: $token');
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    requestPermission();
+    requestToken();
+    subscribeTopic();
+    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+      print('Got a message whilst in the foreground!');
+      print('Message data: ${message.data}');
+
+      if (message.notification != null) {
+        print('Message also contained a notification: ${message.notification}');
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
